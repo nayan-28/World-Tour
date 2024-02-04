43959ecb46257458930b54632ad9527572eac15a
nayan-28 <nayanmalakar720@gmail.com>
2024-02-04 20:20:38 +0600

    display data

--- src/assets/Components/Countries.jsx
+++ src/assets/Components/Countries.jsx
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from "react";
+import Country from "./Country/Country";
 
 const Countries = (props) => {
   const [countries, setCountries] = useState([]);
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => setCountries(data));
   }, []);
-  return <h3>This is Country</h3>;
+  return (
+    <div>
+      <h3>Countries: {countries.length}</h3>
+      {countries.map((country) => (
+        <Country key={country.cca3} country={country}></Country>
+      ))}
+    </div>
+  );
 };
 
 export default Countries;
---
+++ src/assets/Components/Country/Country.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Country.css";
+const Country = ({ country }) => {
+  const { name, flags } = country;
+  console.log(country);
+  return (
+    <div className="country">
+      <h3>Name:{name?.common}</h3>
+      <img src={flags.png} alt="" />
+    </div>
+  );
+};
+
+export default Country;
